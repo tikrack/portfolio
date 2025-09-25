8bdd978e3ea18f063b8574f8058d58d2acf70e13
tikrack <tikrackcode@gmail.com>
2025-09-25 18:35:06 +0330

    [Start Menu]

--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -1,7 +1,9 @@
 const TaskBar = () => {
     return <>
         <footer className={"taskbar-bg h-7.5 fixed bottom-0 right-0 w-screen"}>
-
+            <button className={"cursor-pointer active:brightness-75 focus-visible:outline-none"}>
+                <img src="/start.png" alt=""/>
+            </button>
         </footer>
     </>
 }
